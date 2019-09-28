74b94d4a521c70c435ea359d34cbcfb05cdfdb32
Michael Nagy <michaelnt8@gmail.com>
2019-09-28 13:39:23 -0300

    first screen & testing function & variables

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import './screens/home.dart';
+
+void main() => runApp(new FlutterApp());
+
+class FlutterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter App",
+      home: Scaffold(appBar: AppBar(title: Text("Title")), body: Home()),
+    );
+  
+  }
+  
+}
